6942954cc8ba7796d1d4a00eb8ff8a6e7d479db2
zeroskill2400 <zeroskill2400@gmail.com>
2024-06-08 10:29:52 +0900

    feature(member): password 일치 검사 password 일치 검사 추가 password validation이 빈 필드 체크보다 먼저 작동하는점 수정

--- src/main/java/com/zeroskill/buytopia/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/zeroskill/buytopia/exceptionhandler/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.zeroskill.buytopia.exceptionhandler;
 
+import com.zeroskill.buytopia.exception.EmptyFieldException;
+import com.zeroskill.buytopia.exception.InvalidEmailFormatException;
+import com.zeroskill.buytopia.exception.PasswordMismatchException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,5 +28,26 @@ public class GlobalExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
         return errors;
+    }
+
+    @ResponseStatus
+    @ExceptionHandler(PasswordMismatchException.class)
+    @ResponseBody
+    public ResponseEntity<String> handlePasswordMismatchException(PasswordMismatchException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ResponseStatus
+    @ExceptionHandler(EmptyFieldException.class)
+    @ResponseBody
+    public ResponseEntity<String> handleEmptyFieldException(EmptyFieldException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ResponseStatus
+    @ExceptionHandler(InvalidEmailFormatException.class)
+    @ResponseBody
+    public ResponseEntity<String> handleInvalidEmailFormatException(InvalidEmailFormatException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
