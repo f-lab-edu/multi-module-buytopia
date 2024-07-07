513eb253f49c032e1e0594e5d7367a660d47a7c3
zeroskill2400 <zeroskill2400@gmail.com>
2024-07-07 19:51:19 +0900

    refactor(common): 약관 조회 파라미터 변경 약관 조회 파라미터 enum 및 urlParameter로 변경

--- src/main/java/com/zeroskill/buytopia/controller/TermController.java
+++ src/main/java/com/zeroskill/buytopia/controller/TermController.java
@@ -3,7 +3,7 @@ package com.zeroskill.buytopia.controller;
 import com.zeroskill.buytopia.dto.AgreementDto;
 import com.zeroskill.buytopia.dto.TermDto;
 import com.zeroskill.buytopia.dto.request.AgreeToTermsRequest;
-import com.zeroskill.buytopia.dto.request.GetTermsByTermIdsRequest;
+import com.zeroskill.buytopia.dto.request.PurposeRequest;
 import com.zeroskill.buytopia.dto.response.ApiResponse;
 import com.zeroskill.buytopia.service.TermService;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +18,10 @@ public class TermController {
 
     private final TermService termService;
 
-    @GetMapping("/")
-    // TODO: GET method사용시 파라미터를 urlParameter(?)로 받아주기
-    public ApiResponse<List<TermDto>> getTermsByIds(@RequestBody GetTermsByTermIdsRequest request) {
+    @GetMapping("")
+    public ApiResponse<List<TermDto>> getTermsByIds(@RequestParam("purpose") PurposeRequest request) {
         // TODO: 가장 최신의 액티브인 버전
-        request.check();
-        List<TermDto> termDtos = termService.getTermsByPurpose(request.purpose());
+        List<TermDto> termDtos = termService.getTermsByPurpose(request.name());
         return ApiResponse.of(termDtos);
     }
 
