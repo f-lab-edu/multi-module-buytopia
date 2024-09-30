e57e0a22c4eb83d4d89ed47c4ae003a542a3a372
zeroskill2400 <zeroskill2400@gmail.com>
2024-09-30 17:54:57 +0900

    refactor(order): orderservice - kafka 데이터 누락 수정 - 빌드 에러 수정

--- api/eureka-api/src/main/java/com/zeroskill/eureka_api/EurekaApiApplication.java
+++ api/eureka-api/src/main/java/com/zeroskill/eureka_api/EurekaApiApplication.java
@@ -1,4 +1,4 @@
-package com.zeroskill.eurekaapi;
+package com.zeroskill.eureka_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
